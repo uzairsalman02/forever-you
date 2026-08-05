393178995fd92e09107aa63e18bc491b25574d02
Uzair Salman <uzairsalman66@gmail.com>
2026-08-06 00:08:23 +0500

    Support Vercel root deployment and GitHub Pages subpath deployment seamlessly

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isGithubPages = process.env.GITHUB_PAGES === "true" || process.env.npm_lifecycle_event === "deploy";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/forever-you" : "",
+  basePath: isGithubPages ? "/forever-you" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
